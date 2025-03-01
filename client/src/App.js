b1257cf2b5a7fee610329f0771b614ce0131e053
gauteferstad <gaute.ferstad@gmail.com>
2025-03-01 18:38:30 +0100

    Re-added client folder with all files

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to Video Social Network</h1>
+      <p>Post videos, engage, and connect with others!</p>
+    </div>
+  );
+}
+
+export default App;
